be6c9fe26f1ecb19e4d49777b271f4703b3cb15b
donghyukkil <asterism90@gmail.com>
2023-08-18 14:01:35 +0900

    feat: ImageUploader 생성

---
+++ src/components/ImageUploader/index.jsx
@@ -0,0 +1,24 @@
+const ImageUploader = ({ onUpload }) => {
+  const handleImageChange = async event => {
+    const file = event.target.files[0];
+
+    if (file) {
+      const base64String = await convertToBase64(file);
+      onUpload(base64String);
+    }
+  };
+
+  const convertToBase64 = file => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+
+      reader.onload = () => resolve(reader.result.split(",")[1]);
+      reader.onerror = error => reject(error);
+    });
+  };
+
+  return <input type="file" onChange={handleImageChange} />;
+};
+
+export default ImageUploader;
