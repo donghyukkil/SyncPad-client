70dcc81569d7b1e3e3e34a9d5c617b714d290a64
donghyukkil <asterism90@gmail.com>
2023-08-19 11:22:48 +0900

    feat: 이미지 업로드 전단계 추가

--- src/components/ImageUploader/index.jsx
+++ src/components/ImageUploader/index.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
+
+import Button from "../Button";
+
 const ImageUploader = ({ onUpload }) => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [uploadedImage, setUploadedImage] = useState(null);
+
   const handleImageChange = async event => {
     const file = event.target.files[0];
 
     if (file) {
       const base64String = await convertToBase64(file);
-      URL.createObjectURL(file);
-      onUpload(base64String);
+      setSelectedImage(base64String);
+    }
+  };
+
+  const handleSaveImage = () => {
+    if (selectedImage) {
+      onUpload(selectedImage);
+      setUploadedImage(selectedImage);
     }
   };
 
@@ -21,7 +34,26 @@ const ImageUploader = ({ onUpload }) => {
 
   return (
     <>
-      <input type="file" onChange={handleImageChange} />
+      {selectedImage ? (
+        <div className="flex justify-around">
+          <img
+            src={`data:image/jpeg;base64,${selectedImage}`}
+            alt="Uploaded"
+            className="w-full"
+          />
+
+          <Button onClick={handleSaveImage} style={"bg-purple-800 text-white"}>
+            저장
+          </Button>
+        </div>
+      ) : (
+        <input type="file" onChange={handleImageChange} />
+      )}
+      {uploadedImage && (
+        <div>
+          <p className="text-white">이미지가 업로드되었습니다.</p>
+        </div>
+      )}
     </>
   );
 };
