3579474b85f26e2bfc88a440c3f3ab987df84de7
donghyukkil <asterism90@gmail.com>
2023-08-19 11:51:23 +0900

    feat: PNG 다운로드 기능 추가

--- src/components/LegalPadTextArea/index.jsx
+++ src/components/LegalPadTextArea/index.jsx
@@ -1,10 +1,44 @@
 import { useState, useRef } from "react";
 
 import Button from "../Button";
-
 const LegalpadTextarea = () => {
   const [textValue, setTextValue] = useState("");
   const textareaRef = useRef(null);
+
+  const handleDownloadClick = () => {
+    const canvas = document.createElement("canvas");
+    canvas.width = textareaRef.current.offsetWidth;
+    canvas.height = textareaRef.current.offsetHeight;
+    const ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "#f7e79e";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#000";
+
+    for (let i = 0; i < canvas.height; i += 24) {
+      ctx.fillRect(0, i + 21, canvas.width, 1);
+    }
+
+    ctx.fillStyle = "#333";
+    ctx.font = "18px Courier New";
+
+    const lines = textValue.split("\n");
+
+    for (let i = 0; i < lines.length; i++) {
+      ctx.fillText(lines[i], 15, 30 + i * 24);
+    }
+
+    canvas.toBlob(blob => {
+      const url = window.URL.createObjectURL(blob);
+      const link = document.createElement("a");
+
+      link.href = url;
+      link.download = "textarea_content.png";
+      link.click();
+
+      window.URL.revokeObjectURL(url);
+    }, "image/png");
+  };
 
   return (
     <div className="flex justify-around">
@@ -17,6 +51,7 @@ const LegalpadTextarea = () => {
       />
       <Button
         style={"bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg"}
+        onClick={handleDownloadClick}
       >
         Save as PNG
       </Button>
