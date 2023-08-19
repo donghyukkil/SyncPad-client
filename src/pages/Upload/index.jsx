ab74ecc50e08fdce8628d3450aaf6e7ce2e52076
donghyukkil <asterism90@gmail.com>
2023-08-19 10:15:47 +0900

    feat: uploadImageToServer 추가

--- src/pages/Upload/index.jsx
+++ src/pages/Upload/index.jsx
@@ -1,16 +1,36 @@
 import ImageUploader from "../../components/ImageUploader";
 import NavBar from "../../components/NavBar";
 
+import { CONFIG } from "../../constants/config";
+
 const Upload = () => {
+  const uploadImageToServer = async base64String => {
+    try {
+      const response = await fetch(
+        `${CONFIG.BACKEND_SERVER_URL}/users/${localStorage.getItem(
+          "userEmail",
+        )}/upload`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ imageBase64: base64String }),
+        },
+      );
+
+      const data = await response.json();
+    } catch (error) {
+      console.log("Image upload error", error.message);
+    }
+  };
+
   return (
     <div className="flex">
       <NavBar />
-      <div className="bg-amber-400 w-screen flex ">
+      <div className="bg-yellow-300 w-screen flex">
         <div className="m-auto flex w-1/2 bg-blue-400 justify-between">
-          <ImageUploader />
-          <div className="bg-indigo-950 w-4/12 text-center">
-            <button className="text-zinc-200">저장</button>
-          </div>
+          <ImageUploader onUpload={uploadImageToServer} />
         </div>
       </div>
     </div>
