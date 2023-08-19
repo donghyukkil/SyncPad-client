d48e9d3471d43f1f2f3d3c943543e5f86a78ebea
donghyukkil <asterism90@gmail.com>
2023-08-19 11:43:39 +0900

    feat: LegalpadTextArea 연결

--- src/pages/Create/index.jsx
+++ src/pages/Create/index.jsx
@@ -1,11 +1,14 @@
 import NavBar from "../../components/NavBar";
+import LegalpadTextarea from "../../components/LegalpadTextArea";
 
 const Create = () => {
   return (
     <div className="flex">
       <NavBar />
       <div className="bg-yellow-300 w-screen h-screen flex">
-        <div className="flex bg-teal-950 w-6/12 h-2/4 m-auto py-0 justify-center"></div>
+        <div className="flex bg-teal-950 w-6/12 h-2/4 m-auto py-0 justify-center">
+          <LegalpadTextarea />
+        </div>
       </div>
     </div>
   );
