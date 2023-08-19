af2f1f8849aaa556ca6a4fb5e01419cee49ec48e
donghyukkil <asterism90@gmail.com>
2023-08-19 11:31:39 +0900

    feat: Create 라우터 연결

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 import Home from "./pages/Home/index.jsx";
 import Upload from "./pages/Upload";
+import Create from "./pages/Create/index.jsx";
 
 import "./config/firebase-config";
 
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/upload" element={<Upload />} />
+        <Route path="/create" element={<Create />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
