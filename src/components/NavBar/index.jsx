14b3230f12b4df9a3acfcaab52a820b71871eeec
donghyukkil <asterism90@gmail.com>
2023-08-19 10:20:35 +0900

    feat: Button 및 navigate 기능 추가

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,56 +1,75 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import Button from "../Button";
 
 const NavBar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+
+  const navigate = useNavigate();
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
+  const navigateToHome = () => {
+    navigate("/");
+  };
+
+  const navigateToUpload = () => {
+    navigate("/upload");
+  };
+
+  const navigateToCreate = () => {
+    navigate("/create");
+  };
+
   return (
-    <nav className="bg-gray-800 h-screen flex justify-center">
-      <div className="flex flex-col space-y-20">
-        <a
-          href="#"
-          className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
-          aria-current="page"
+    <nav className="bg-blue-950 h-screen flex justify-center w-1/6">
+      <div className="flex flex-col space-y-20 justify-evenly">
+        <Button
+          style={
+            "bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+          }
+          onClick={navigateToHome}
         >
           HOME
-        </a>
-        <a
-          href="#"
-          className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+        </Button>
+        <Button
+          style={
+            "bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+          }
+          onClick={navigateToCreate}
         >
           CREATE
-        </a>
-        <a
-          href="#"
-          className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+        </Button>
+        <Button
+          style={
+            "bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+          }
+          onClick={navigateToUpload}
         >
           UPLOAD
-        </a>
-        <a
-          href="#"
-          className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+        </Button>
+        <Button
+          style={
+            "bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+          }
         >
           MYPAGE
-        </a>
+        </Button>
+
         <div className="mt-2 ml-3">
-          <button
-            type="button"
-            className="relative flex rounded-full bg-gray-800 text-sm"
+          <Button
+            style={"relative flex rounded-full bg-gray-800 text-sm"}
             onClick={toggleMenu}
           >
             <img className="h-8 w-8 rounded-full m-6" src="" alt="" />
-          </button>
+          </Button>
           {menuOpen && (
             <div className="bg-white">
-              <a href="#" className="block text-sm text-gray-700">
-                MYPAGE
-              </a>
-              <a href="#" className="block px-4 py-2 text-sm text-gray-700">
-                LOGOUT
-              </a>
+              <Button style={"block text-sm text-gray-700"}>MYPAGE</Button>
+              <Button style={"block text-sm text-gray-700"}>LOGOUT</Button>
             </div>
           )}
         </div>
