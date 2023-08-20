50e42854433d0c906d57eb45461599827cc0a6ea
donghyukkil <asterism90@gmail.com>
2023-08-20 13:58:50 +0900

    style: div 태그 테두리 수정

--- src/pages/Upload/index.jsx
+++ src/pages/Upload/index.jsx
@@ -29,7 +29,7 @@ const Upload = () => {
     <div className="flex">
       <NavBar />
       <div className="bg-yellow-300 w-screen h-screen flex">
-        <div className="flex bg-teal-950 w-6/12 h-2/4 m-auto py-0 justify-center">
+        <div className="flex bg-teal-950 w-6/12 h-2/4 m-auto py-0 justify-center rounded-md">
           <ImageUploader onUpload={uploadImageToServer} />
         </div>
       </div>
