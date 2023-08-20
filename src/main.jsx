bd9b4dd819fd8425691448231067d17af8411df5
donghyukkil <asterism90@gmail.com>
2023-08-20 15:34:08 +0900

    feat: Mypage 연결

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home/index.jsx";
 import Upload from "./pages/Upload";
 import Create from "./pages/Create/index.jsx";
+import MyPage from "./pages/Mypage/index.jsx";
 
 import "./config/firebase-config";
 
@@ -17,6 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/" element={<Home />} />
         <Route path="/upload" element={<Upload />} />
         <Route path="/create" element={<Create />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
