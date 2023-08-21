9c5957d2f558700d79d8dfee0fd0e6eea0a87ef4
donghyukkil <asterism90@gmail.com>
2023-08-21 11:29:28 +0900

    feat: useStore를 사용하여 동적으로 data 전달

--- src/pages/Mypage/index.jsx
+++ src/pages/Mypage/index.jsx
@@ -1,19 +1,27 @@
+import { useEffect } from "react";
+
 import TextCard from "../../components/TextCard";
 import NavBar from "../../components/NavBar";
+import useStore from "../../usStore";
 
 const Mypage = () => {
+  const { texts, fetchTexts } = useStore();
+
+  useEffect(() => {
+    fetchTexts();
+  }, []);
+
   return (
     <div className="flex">
       <NavBar />
       <div className="bg-yellow-300 w-screen h-screen flex">
         <div className="flex bg-teal-950 w-6/12 h-2/4 m-auto py-0 justify-center rounded-md">
           <main className="flex-grow grid grid-cols-3 grid-rows-2 gap-4 p-4">
-            <TextCard></TextCard>
-            <TextCard></TextCard>
-            <TextCard></TextCard>
-            <TextCard></TextCard>
-            <TextCard></TextCard>
-            <TextCard></TextCard>
+            {texts.data !== undefined
+              ? texts.data.map((text, index) => (
+                  <TextCard key={index} text={text} />
+                ))
+              : null}
           </main>
         </div>
       </div>
