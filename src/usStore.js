8ec84d62642186d273e0ddc6118b77824300398e
donghyukkil <asterism90@gmail.com>
2023-08-21 11:28:36 +0900

    feat: zustand store 설정

---
+++ src/usStore.js
@@ -0,0 +1,24 @@
+import { create } from "zustand";
+
+import { CONFIG } from "./constants/config";
+
+const useStore = create(set => ({
+  texts: {},
+  setTexts: texts => set({ texts }),
+  fetchTexts: async () => {
+    try {
+      const response = await fetch(
+        `${CONFIG.BACKEND_SERVER_URL}/users/${localStorage.getItem(
+          "userEmail",
+        )}/texts`,
+      );
+
+      const texts = await response.json();
+      set({ texts });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+}));
+
+export default useStore;
