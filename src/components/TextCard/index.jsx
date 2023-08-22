704824e6f7f6684605406410cf3001d6306e5499
donghyukkil <asterism90@gmail.com>
2023-08-22 14:58:03 +0900

    feat: detailPage 이동 기능 추가

--- src/components/TextCard/index.jsx
+++ src/components/TextCard/index.jsx
@@ -1,6 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 const TextCard = ({ text }) => {
+  const navigate = useNavigate();
+
+  const navigateToDetailPage = () => {
+    navigate(`/mypage/${text._id}`);
+  };
+
+  const createdAt = new Date(text.createdAt).toLocaleString();
+
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col" onClick={navigateToDetailPage}>
       <div className="bg-amber-700 w-full h-full"></div>
       <div className="bg-yellow-300 w-full h-screen flex">
         <div className="flex flex-col justify-around">
