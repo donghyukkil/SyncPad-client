1828491dbe89398052c6086ec359cb3f567ab44a
donghyukkil <asterism90@gmail.com>
2023-08-22 15:15:28 +0900

    feat: TextCardDetail 라우터 연결

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Home from "./pages/Home/index.jsx";
 import Upload from "./pages/Upload";
 import Create from "./pages/Create/index.jsx";
 import MyPage from "./pages/Mypage/index.jsx";
+import TextCardDetail from "./components/TextCardDetail/index.jsx";
 
 import "./config/firebase-config";
 
@@ -19,6 +20,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/upload" element={<Upload />} />
         <Route path="/create" element={<Create />} />
         <Route path="/mypage" element={<MyPage />} />
+        <Route path="/mypage/:text_id" element={<TextCardDetail />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
