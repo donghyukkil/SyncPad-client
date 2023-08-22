d492c6dddfccfc0411a21b87d8e8ddf579979a22
donghyukkil <asterism90@gmail.com>
2023-08-22 14:56:47 +0900

    refactor: TextCard 정적페이지 수정

--- src/components/TextCard/index.jsx
+++ src/components/TextCard/index.jsx
@@ -1,13 +1,17 @@
-import LegalPadTextArea from "../LegalPadTextArea";
-
 const TextCard = ({ text }) => {
   return (
-    <div>
-      <div>image</div>
-      <div className="text-white h-4/5 w-full overflow-hidden">
-        <LegalPadTextArea text={text.content} />
+    <div className="flex flex-col">
+      <div className="bg-amber-700 w-full h-full"></div>
+      <div className="bg-yellow-300 w-full h-screen flex">
+        <div className="flex flex-col justify-around">
+          <div
+            className="bg-white m-auto rounded-md overflow-hidden text-sm"
+            style={{ width: "80px", height: "80px" }}
+          >
+            {text.content}
+          </div>
+        </div>
       </div>
-      ;
     </div>
   );
 };
