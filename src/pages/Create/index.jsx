6025a6488fbdc6f9797c2e3055943aee93496533
donghyukkil <asterism90@gmail.com>
2023-08-22 14:30:10 +0900

    chore: 컴포넌트import 이름 변경

--- src/pages/Create/index.jsx
+++ src/pages/Create/index.jsx
@@ -1,5 +1,5 @@
 import NavBar from "../../components/NavBar";
-import LegalpadTextarea from "../../components/LegalpadTextArea";
+import LegalPadTextarea from "../../components/LegalpadTextArea";
 
 const Create = () => {
   return (
@@ -7,7 +7,7 @@ const Create = () => {
       <NavBar />
       <div className="bg-yellow-300 w-screen h-screen flex">
         <div className="flex bg-teal-950 w-6/12 h-2/4 m-auto py-0 justify-center rounded-md">
-          <LegalpadTextarea />
+          <LegalPadTextarea />
         </div>
       </div>
     </div>
