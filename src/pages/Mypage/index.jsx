da0b5f21d68d1f055985d385f80b8a158652821a
donghyukkil <asterism90@gmail.com>
2023-08-23 14:05:18 +0900

    feat: currentPage, totalPages 상태 추가

--- src/pages/Mypage/index.jsx
+++ src/pages/Mypage/index.jsx
@@ -1,21 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import TextCard from "../../components/TextCard";
 import NavBar from "../../components/NavBar";
 import useStore from "../../usStore";
 
 const Mypage = () => {
-  const { texts, fetchTexts } = useStore();
+  const { texts, fetchTexts, currentPage } = useStore();
+  const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
-    fetchTexts();
-  }, []);
+    fetchTexts(currentPage);
+
+    if (texts.totalPages) {
+      setTotalPages(texts.totalPages);
+    }
+  }, [currentPage, texts]);
 
   return (
     <div className="flex">
       <NavBar />
       <div className="bg-yellow-300 w-screen h-screen flex">
-        <div className="flex bg-teal-950 w-6/12 h-2/4 m-auto py-0 justify-center rounded-md">
+        <div className="flex flex-col bg-teal-950 w-6/12 h-2/4 m-auto py-0 justify-center rounded-md">
           <main className="flex-grow grid grid-cols-3 grid-rows-2 gap-4 p-4">
             {texts.data !== undefined
               ? texts.data.map((text, index) => (
