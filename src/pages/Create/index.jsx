a6fa55fe3097ddea6c967876ff3a3823dffff59e
donghyukkil <asterism90@gmail.com>
2023-08-26 14:43:47 +0900

    style: Create 스타일 수정

--- src/pages/Create/index.jsx
+++ src/pages/Create/index.jsx
@@ -5,7 +5,7 @@ const Create = () => {
   return (
     <div className="flex">
       <NavBar />
-      <div className="bg-yellow-300 w-screen h-screen flex">
+      <div className="bg-yellow-300 w-screen h-screen flex flex-col">
         <div className="flex bg-teal-950 w-6/12 h-2/4 m-auto py-0 justify-center rounded-md">
           <LegalPadTextarea />
         </div>
