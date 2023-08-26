c834f84827e756b80f90da56d34ba62be44dcb57
donghyukkil <asterism90@gmail.com>
2023-08-26 21:27:42 +0900

    feat: logout 기능 추가

--- src/components/SubNavBar/index.jsx
+++ src/components/SubNavBar/index.jsx
@@ -1,7 +1,44 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import Button from "../Button";
 
+import { CONFIG } from "../../constants/config";
+
 const SubNavBar = ({}) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const navigate = useNavigate();
+
   const userPhotoURL = localStorage.getItem("userPhotoURL");
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleLogout = async () => {
+    try {
+      const response = await fetch(
+        `${CONFIG.BACKEND_SERVER_URL}/users/logout`,
+        {
+          method: "POST",
+          credentials: "include",
+        },
+      );
+
+      if (response.ok) {
+        console.log("로그아웃 성공");
+      } else {
+        console.error("로그아웃 실패");
+      }
+
+      localStorage.clear();
+
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="mr-10 bg-amber-700 h-16">
@@ -11,8 +48,19 @@ const SubNavBar = ({}) => {
             className="h-12 w-12 rounded-full"
             src={userPhotoURL}
             alt="userPhoto"
+            onClick={toggleMenu}
           />
         </Button>
+        {menuOpen && (
+          <div className="bg-white">
+            <Button
+              style={"block text-sm text-gray-700"}
+              onClick={handleLogout}
+            >
+              LOGOUT
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   );
