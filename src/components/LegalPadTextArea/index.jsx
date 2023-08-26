c8e6c818e7c88356d20aa8f609ce0fc170734e62
donghyukkil <asterism90@gmail.com>
2023-08-26 18:52:38 +0900

    refactor: textarea 영역 크기 변경

--- src/components/LegalPadTextArea/index.jsx
+++ src/components/LegalPadTextArea/index.jsx
@@ -71,7 +71,7 @@ const LegalPadTextArea = () => {
         <div className="relative">
           <div className="bg-amber-700 w-full h-1/5 rounded-md"></div>
           <textarea
-            className="p-3 bg-yellow-200 border border-gray-400 rounded-lg h-48 resize-none"
+            className="p-3 bg-yellow-200 border border-gray-400 rounded-lg h-72 resize-none"
             ref={textareaRef}
             onChange={event => setTextValue(event.target.value)}
             style={{
