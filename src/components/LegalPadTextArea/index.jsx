9031bbdc05850d06e9025007fdf5d0f2c27e90b0
donghyukkil <asterism90@gmail.com>
2023-08-27 23:30:09 +0900

    refactor: 렌더링 태그를 textarea에서 ul/li태그를 이용한 방식으로 변경

--- src/components/LegalPadTextArea/index.jsx
+++ src/components/LegalPadTextArea/index.jsx
@@ -1,4 +1,6 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
+
+import { useNavigate } from "react-router-dom";
 
 import useStore from "../../useStore";
 
@@ -8,31 +10,39 @@ import { CONFIG } from "../../constants/config";
 
 const LegalPadTextArea = () => {
   const { textValue, setTextValue } = useStore();
+  const [messages, setMessages] = useState([]);
 
-  const textareaRef = useRef(null);
+  const captureDivRef = useRef(null);
+  const navigate = useNavigate();
+
+  const navigateToMypage = () => {
+    navigate("/mypage");
+  };
 
   const handleDownloadClick = () => {
     const lineHeight = 36;
     const padding = 15;
-    const lines = textValue.split("\n");
-    const lineCount = lines.length;
-    const canvasHeight = lineCount * lineHeight + 2 * padding;
+
+    const canvasWidth = captureDivRef.current.offsetWidth;
+    const canvasHeight = captureDivRef.current.offsetHeight;
 
     const canvas = document.createElement("canvas");
-    canvas.width = textareaRef.current.offsetWidth;
+    canvas.width = canvasWidth;
     canvas.height = canvasHeight;
 
     const ctx = canvas.getContext("2d");
+
     ctx.fillStyle = "#f7e79e";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
     ctx.fillStyle = "#000";
     ctx.font = "18px Courier New";
 
-    for (let i = 0; i < lines.length; i++) {
+    messages.forEach((message, i) => {
       const y = padding + i * lineHeight;
-      ctx.fillText(lines[i], padding, y + lineHeight);
-      ctx.fillRect(padding, y + lineHeight - 1, canvas.width - 2 * padding, 1);
-    }
+      ctx.fillText(message, padding, y + lineHeight);
+      ctx.fillRect(padding, y + lineHeight - 1, canvasWidth - 2 * padding, 1);
+    });
 
     canvas.toBlob(blob => {
       const url = window.URL.createObjectURL(blob);
@@ -48,6 +58,17 @@ const LegalPadTextArea = () => {
 
   const createTextToServer = async () => {
     try {
+      const divContent = captureDivRef.current.innerHTML;
+
+      const regex = /<li[^>]*>([^<]+)<\/li>/g;
+
+      let matches;
+      let results = [];
+
+      while ((matches = regex.exec(divContent)) !== null) {
+        results.push(matches[1]);
+      }
+
       const response = await fetch(
         `${CONFIG.BACKEND_SERVER_URL}/users/${localStorage.getItem(
           "userEmail",
@@ -57,12 +78,20 @@ const LegalPadTextArea = () => {
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({ content: textValue }),
+          body: JSON.stringify({ content: results }),
         },
       );
+
+      navigateToMypage();
     } catch (error) {
       console.log("Image upload error", error.message);
     }
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setMessages([...messages, textValue]);
+    setTextValue("");
   };
 
   return (
@@ -72,18 +101,37 @@ const LegalPadTextArea = () => {
           <div className="bg-amber-700 w-full h-1/6 rounded-md text-center line leading-10 text-2xl">
             Hello, legalPad!
           </div>
-          <textarea
-            className="p-3 bg-yellow-200 border border-gray-400 rounded-lg h-72 resize-none"
-            ref={textareaRef}
-            onChange={event => setTextValue(event.target.value)}
-            style={{
-              lineHeight: "34px",
-              fontFamily: "Courier New",
-              color: "#000",
-              width: "100%",
-            }}
-            placeholder="메모하고 싶은 내용을 적고 저장 버튼이나 다운로드 버튼을 누르세요"
-          />
+          <div
+            className="p-3 bg-yellow-200 border border-gray-400 rounded-lg h-72 overflow-y-auto"
+            ref={captureDivRef}
+          >
+            <ul>
+              {messages.map((message, index) => (
+                <li
+                  key={index}
+                  style={{
+                    lineHeight: "34px",
+                    fontFamily: "Courier New",
+                    color: "#000",
+                  }}
+                >
+                  {message}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <input
+              className="p-3 bg-yellow-200 border border-gray-400 rounded-lg w-full"
+              value={textValue}
+              onChange={event => setTextValue(event.target.value)}
+              style={{
+                fontFamily: "Courier New",
+                color: "#000",
+              }}
+              placeholder="메모하고 싶은 내용을 적고 저장 버튼이나 다운로드 버튼을 누르세요"
+            />
+          </form>
         </div>
         <Button
           style="bg-sky-400 hover:bg-blue-600 text-black px-4 py-2 rounded-lg p-0"
