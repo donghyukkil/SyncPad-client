aa90e18d5f5e3ea44c5e6993275de75a42720487
donghyukkil <asterism90@gmail.com>
2023-08-28 00:02:16 +0900

    feat: Chatting에 handleDownloadClick 추가

--- src/pages/Chatting/index.jsx
+++ src/pages/Chatting/index.jsx
@@ -5,12 +5,15 @@ import { io } from "socket.io-client";
 import { CONFIG } from "../../constants/config";
 import NavBar from "../../components/NavBar";
 import SubNavBar from "../../components/SubNavBar";
+import Button from "../../components/Button";
 
 const Chatting = () => {
   const [text, setText] = useState("");
   const [messages, setMessages] = useState([]);
 
   const socket = useRef();
+
+  const captureDivRef = useRef(null);
 
   const stringToColor = string => {
     let hash = 0;
@@ -37,6 +40,43 @@ const Chatting = () => {
       setMessages(prevMessage => [...prevMessage, text]);
       setText("");
     }
+  };
+
+  const handleDownloadClick = () => {
+    const lineHeight = 36;
+    const padding = 15;
+
+    const canvasWidth = captureDivRef.current.offsetWidth;
+    const canvasHeight = captureDivRef.current.offsetHeight;
+
+    const canvas = document.createElement("canvas");
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+
+    const ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "#f7e79e";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "#000";
+    ctx.font = "18px Courier New";
+
+    messages.forEach((message, i) => {
+      const y = padding + i * lineHeight;
+      ctx.fillText(message, padding, y + lineHeight);
+      ctx.fillRect(padding, y + lineHeight - 1, canvasWidth - 2 * padding, 1);
+    });
+
+    canvas.toBlob(blob => {
+      const url = window.URL.createObjectURL(blob);
+      const link = document.createElement("a");
+
+      link.href = url;
+      link.download = "textarea_content.png";
+      link.click();
+
+      window.URL.revokeObjectURL(url);
+    }, "image/png");
   };
 
   useEffect(() => {
@@ -73,6 +113,7 @@ const Chatting = () => {
                 fontSize: "20px",
                 fontFamily: "Courier New",
               }}
+              ref={captureDivRef}
             >
               <ul>
                 {messages.map((message, index) => (
@@ -101,6 +142,12 @@ const Chatting = () => {
                 className="text-center h-14"
               />
             </form>
+            <Button
+              style="bg-sky-400 hover:bg-blue-600 text-black px-4 py-2 rounded-lg p-0"
+              onClick={handleDownloadClick}
+            >
+              다운로드
+            </Button>
           </div>
         </div>
       </div>
