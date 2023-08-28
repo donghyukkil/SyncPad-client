64dedb216aa6732ac59a5978e2cdd49055582ec8
donghyukkil <asterism90@gmail.com>
2023-08-28 17:20:14 +0900

    refactor: handleLogout 제거

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -29,28 +29,6 @@ const NavBar = () => {
     navigate("/chat");
   };
 
-  const handleLogout = async () => {
-    try {
-      const response = await fetch(
-        `${CONFIG.BACKEND_SERVER_URL}/users/logout`,
-        {
-          method: "POST",
-          credentials: "include",
-        },
-      );
-
-      if (response.ok) {
-        console.log("로그아웃 성공");
-      } else {
-        console.error("로그아웃 실패");
-      }
-      localStorage.clear();
-      navigate("/");
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <nav className="bg-blue-950 h-screen flex justify-center w-1/6">
       <div className="flex flex-col space-y-20 justify-evenly">
