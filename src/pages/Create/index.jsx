90fe676e70db792e2f6ba6717fc02ce84a7d5223
donghyukkil <asterism90@gmail.com>
2023-09-01 18:42:17 +0900

    refactor: Created에서 TextCardDetail 도입

--- src/pages/Create/index.jsx
+++ src/pages/Create/index.jsx
@@ -1,22 +1,13 @@
-import NavBar from "../../components/NavBar";
-import LegalPadTextarea from "../../components/LegalPadTextArea";
-import SubNavBar from "../../components/SubNavBar";
+import TextCardDetail from "../../components/TextCardDetail";
 
 const Create = () => {
   return (
     <div className="flex">
-      <NavBar />
       <div
-        className="w-screen h-screen flex flex-col"
-        style={{ backgroundColor: "#F8F0E5" }}
+        className="flex w-3/4 h-3/4 m-auto justify-center rounded-md"
+        style={{ backgroundColor: "#DAC0A3" }}
       >
-        <SubNavBar />
-        <div
-          className="flex w-3/4 h-3/4 m-auto justify-center rounded-md"
-          style={{ backgroundColor: "#DAC0A3" }}
-        >
-          <LegalPadTextarea />
-        </div>
+        <TextCardDetail />
       </div>
     </div>
   );
