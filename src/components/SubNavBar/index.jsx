da927ab8886a1e70d65b6ca81dffa768d52a2192
donghyukkil <asterism90@gmail.com>
2023-09-05 11:31:49 +0900

    feat: 방 생성/삭제 기능 추가

--- src/components/SubNavBar/index.jsx
+++ src/components/SubNavBar/index.jsx
@@ -7,7 +7,7 @@ import Button from "../Button";
 
 import { CONFIG } from "../../constants/config";
 
-const SubNavBar = ({ roomId, setRoomId }) => {
+const SubNavBar = ({ roomId, setRoomId, text_id, createNewRoom }) => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [rooms, setRooms] = useState([
     { roomId: "room1", roomName: "공개 채팅 방" },
@@ -139,32 +139,41 @@ const SubNavBar = ({ roomId, setRoomId }) => {
 
   return (
     <div
-      className="mr-10 h-16 flex justify-end relative w-full"
+      className="mr-10 h-16 flex justify-between relative w-full"
       style={{ backgroundColor: "#DAC0A3" }}
     >
-      {roomId && (
-        <div className="m-4">
-          <Button
-            style="bg-white hover:border-0 hover:bg-gray-100 text-black px-4 py-2 rounded-md text-center text-lg font-semibold font-mono mt-8"
-            onClick={deleteRoom}
-          >
-            방 삭제
-          </Button>
-          <select
-            onChange={e => setRoomId(e.target.value)}
-            className="p-2 border rounded"
-          >
-            <option value="" disabled>
-              Select a room
+      <div className="flex space-x-10 m-auto ml-20">
+        <select
+          onChange={e => {
+            const selectedRoomId = e.target.value;
+            setRoomId(selectedRoomId);
+            handleRoomClick(selectedRoomId);
+          }}
+          className="p-2 border rounded"
+        >
+          <option key="default-option" value="" disabled>
+            Select a room
+          </option>
+          {rooms.map((room, index) => (
+            <option key={index} value={room.roomId}>
+              {room.roomName}
             </option>
-            {rooms.map(room => (
-              <option key={room.roomId} value={room.roomId}>
-                {room.roomName}
-              </option>
-            ))}
-          </select>
-        </div>
-      )}
+          ))}
+        </select>
+        <Button
+          style="bg-white hover:border-0 hover:bg-gray-100 text-black px-2 rounded-md text-center text-lg font-semibold font-mono"
+          onClick={createNewRoom}
+        >
+          방 생성
+        </Button>
+        <Button
+          style="bg-white hover:border-0 hover:bg-gray-100 text-black px-2 rounded-md text-center text-lg font-semibold font-mono"
+          onClick={deleteRoom}
+        >
+          방 삭제
+        </Button>
+      </div>
+
       <div className="flex flex-col mr-32 my-2">
         {userPhotoURL ? (
           <>
