2a8326a243dd02cfe8641eaa9a3695a2cb932827
donghyukkil <asterism90@gmail.com>
2023-11-11 15:55:14 +0900

    chore: NavBar 버튼 문구 수정

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -21,7 +21,7 @@ const NavBar = () => {
           }
           onClick={() => navigateTo("/create")}
         >
-          새 메모
+          메모 만들기
         </Button>
         <Button
           style={
@@ -29,7 +29,7 @@ const NavBar = () => {
           }
           onClick={() => navigateTo("/upload")}
         >
-          이미지 메모
+          메모 추출하기
         </Button>
         <Button
           style={
@@ -38,14 +38,6 @@ const NavBar = () => {
           onClick={() => navigateTo("/mypage")}
         >
           마이 페이지
-        </Button>
-        <Button
-          style={
-            "bg-gray-900 text-white rounded-md px-3 py-2 text-lg font-semibold font-mono"
-          }
-          onClick={() => navigateTo("/chat")}
-        >
-          함께 메모
         </Button>
       </div>
     </nav>
